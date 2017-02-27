eb3490538a63099cee5242ff5f05144899bbb702
Andrew Becker <andrew.d.becker@lowes.com>
2017-02-27 08:49:37 -0500

    Updated item cards

--- src/components/item_card.js
+++ src/components/item_card.js
@@ -7,17 +7,17 @@ class ItemCard extends Component {
     const { item } = this.props;
 
     return (
-        // <div className="col-sm-6 col-md-4 col-lg-2">
-          <div className="card" style={{width: '170px'}}>
-              <img className="card-img-top" src={item.MainImage.url_170x135} />
+        <div className="col-sm-6 col-md-4 col-lg-3" style={{marginTop: '20px'}}>
+              <div style={{width: '100%'}}>
+                <img className="card-img-top product_thumb" src={item.MainImage.url_170x135} style={{width: '100%'}} />
+              </div>
               <div className="card-block">
-                <h5 className="card-title">{item.title.replace(/&quot;/g, '"')}</h5>
+                <h5 className="card-title">{item.title.replace(/&quot;/g, '"')}</h5> {/*Use css text-overflow: ellipsis on here*/}
               </div>
               <div className="card-footer">
-                <a href={`/listing/${item.url}`} className="btn btn-primary">More Info</a>
+                <a href={`/listing/${item.listing_id}`} className="btn btn-primary">More Info</a>
               </div>
           </div>
-        // </div>
     );
   }
 }
