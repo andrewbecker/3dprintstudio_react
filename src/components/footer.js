7f30f490d439974b0a0c746a98bf297484447f36
Andrew Becker <andrew.d.becker@lowes.com>
2017-02-27 15:46:03 -0500

    Adding footer

---
+++ src/components/footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="mt-5 pt-3 row" style={{borderTop: '1px solid #BBB'}}>
+          <div className="col-xl-12 text-center">
+            
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
